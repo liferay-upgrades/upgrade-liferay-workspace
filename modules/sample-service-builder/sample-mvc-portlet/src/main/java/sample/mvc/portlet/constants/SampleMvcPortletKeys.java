3f9691164b4beb3d78aaf13a5fccd001d940c25f
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-11-08 15:44:52 -0300

    Add MVCPortlet Sample

---
+++ modules/sample-service-builder/sample-mvc-portlet/src/main/java/sample/mvc/portlet/constants/SampleMvcPortletKeys.java
@@ -0,0 +1,11 @@
+package sample.mvc.portlet.constants;
+
+/**
+ * @author me
+ */
+public class SampleMvcPortletKeys {
+
+	public static final String SAMPLEMVC =
+		"sample_mvc_portlet_SampleMvcPortlet";
+
+}
